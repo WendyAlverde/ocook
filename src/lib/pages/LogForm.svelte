a68c7a79abaf1aeffc76e0e0864f2650c3c1bac9
WendyAlverde <wendy.alverde@oclock.school>
2024-04-04 21:05:57 +0200

    add-CSS

--- src/lib/pages/LogForm.svelte
+++ src/lib/pages/LogForm.svelte
@@ -2,8 +2,14 @@
     <h1>Formulaires</h1>
     <section class="backgroundcomponent">
         <h2>Se connecter</h2>
+        <form action="">
+
+        </form>
     </section>
     <section class="backgroundcomponent">
         <h2>S'enregistrer</h2>
+        <form action="">
+            
+        </form>
     </section>
 </main>
--- src/lib/pages/Recipes.svelte
+++ src/lib/pages/Recipes.svelte
@@ -7,75 +7,75 @@
 
     <section class="backgroundsection">
         <article class="framerecipes">
-            <img class="recipes" src="nom-du-ficher" alt="première recette" />
+            <img class="recipes" src="../public/images/foods/apero.webp" alt="première recette" />
             <h3>Chili con carne</h3> <!--recipe title-->
             <p>Wendy Alverde</p> <!--author name-->
             <p>Plat</p> <!--category-->
             <a href="/recipes" use:link>Voir plus</a>
         </article>
         <article class="framerecipes">
-            <img class="recipes" src="nom-du-ficher" alt="première recette" />
+            <img class="recipes" src="../public/images/foods/apero.webp" alt="première recette" />
             <h3>Entrecôte</h3> <!--recipe title-->
             <p>Nicolas Ugé</p> <!--author name-->
             <p>Plat</p> <!--category-->
             <a href="/recipes" use:link>Voir plus</a>
         </article>
         <article class="framerecipes">
-            <img class="recipes" src="nom-du-ficher" alt="première recette" />
+            <img class="recipes" src="../public/images/foods/pates.webp" alt="première recette" />
             <h3>Chili con carne</h3> <!--recipe title-->
             <p>Wendy Alverde</p> <!--author name-->
             <p>Plat</p> <!--category-->
             <a href="/recipes" use:link>Voir plus</a>
         </article>
         <article class="framerecipes">
-            <img class="recipes" src="nom-du-ficher" alt="première recette" />
+            <img class="recipes" src="../public/images/foods/tarte.webp" alt="première recette" />
             <h3>Entrecôte</h3> <!--recipe title-->
             <p>Nicolas Ugé</p> <!--author name-->
             <p>Plat</p> <!--category-->
             <a href="/recipes" use:link>Voir plus</a>
         </article>
 
-        <div class="recipes-pagination">
-            <button role="button" aria-pressed="false">1</button>
-            <button role="button" aria-pressed="false">2</button>
-            <button role="button" aria-pressed="false">3</button>
-            <button role="button" aria-pressed="false">4</button>
-            <button role="button" aria-pressed="false">5</button>
-            <button role="button" aria-pressed="false">6</button>
+        <div>
+            <button class="pagination" role="button" aria-pressed="false">1</button>
+            <button class="pagination" role="button" aria-pressed="false">2</button>
+            <button class="pagination" role="button" aria-pressed="false">3</button>
+            <button class="pagination" role="button" aria-pressed="false">4</button>
+            <button class="pagination" role="button" aria-pressed="false">5</button>
+            <button class="pagination" role="button" aria-pressed="false">6</button>
         </div>
-
     </section>
 
     <section class="backgroundsection">
-        <button role="button" aria-pressed="false">Ajouter une recette ?</button>
+        <button role="button" class="formrecipes" aria-pressed="false">Ajouter une recette ?</button>
     </section>
 
     <section class="backgroundsection">
         <h2 class="title">Les filtres des recettes</h2>
-        <button role="button" aria-pressed="false">
+        <button role="button" class="filter" aria-pressed="false">
             <img class="categories" src="nom-du-ficher" alt="apéritif" />
             <p>Appéritif</p>
         </button>
-        <button role="button" aria-pressed="false">
+        <button role="button" class="filter" aria-pressed="false">
             <img class="categories" src="nom-du-ficher" alt="entrée" />
             <p>Entrée</p>
         </button>
-        <button role="button" aria-pressed="false">
+        <button role="button" class="filter" aria-pressed="false">
             <img class="categories" src="nom-du-ficher" alt="plat" />
             <p>Plat</p>
         </button>
-        <button role="button" aria-pressed="false">
+        <button role="button" class="filter" aria-pressed="false">
             <img class="categories" src="nom-du-ficher" alt="dessert" />
             <p>Dessert</p>
         </button>
-        <button role="button" aria-pressed="false">
+        <button role="button"class="filter" aria-pressed="false">
             <img class="categories" src="nom-du-ficher" alt="viande" />
             <p>Viande</p>
         </button>
-        <button role="button" aria-pressed="false">
+        <button role="button"class="filter" aria-pressed="false">
             <img class="categories" src="nom-du-ficher" alt="végétalien" />
             <p>Végétalien</p>
         </button>     
     </section>
-
 </main>
+
+
