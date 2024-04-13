4865686337d57c82d003af95c3b5bd92d2a52b5f
WendyAlverde <148342924+WendyAlverde@users.noreply.github.com>
2024-04-13 22:18:48 +0200

    update-import-filters

--- src/lib/components/Filter.svelte
+++ src/lib/components/Filter.svelte
@@ -1,10 +1,10 @@
 <script>
-    import imageApero from "../../../src/assets/images/logos/martini-glass-citrus-solid.svg"
-    import imageEntree from "../../../src/assets/images/logos/bowl-rice-solid.svg"
-    import imagePlat from "../../../src/assets/images/logos/utensils-solid.svg"
-    import imageDessert from "../../../src/assets/images/logos/ice-cream-solid.svg"
-    import imageViande from "../../../src/assets/images/logos/drumstick-bite-solid.svg"
-    import imageVegetalien from "../../../src/assets/images/logos/carrot-solid.svg"
+    import imageApero from "../../../src/assets/images/logos/filters/martini-glass-citrus-solid.svg"
+    import imageEntree from "../../../src/assets/images/logos/filters/bowl-rice-solid.svg"
+    import imagePlat from "../../../src/assets/images/logos/filters/utensils-solid.svg"
+    import imageDessert from "../../../src/assets/images/logos/filters/ice-cream-solid.svg"
+    import imageViande from "../../../src/assets/images/logos/filters/drumstick-bite-solid.svg"
+    import imageVegetalien from "../../../src/assets/images/logos/filters/carrot-solid.svg"
 </script>
 
 <section class="backgroundsection">
