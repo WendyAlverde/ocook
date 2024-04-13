4ed047c4956209fd51528b6e59faa716fa7d80b4
WendyAlverde <wendy.alverde@oclock.school>
2024-04-13 20:11:27 +0200

    add-legal-notice

--- src/lib/components/Footer.svelte
+++ src/lib/components/Footer.svelte
@@ -10,12 +10,12 @@
     <img class = "logofooter" src={ocooklogo} alt="Logo O'Cook">
         <nav class = "navfooter" role="navigation">
             <ul class="socialicons">
-                <li><a href="#" target="_blank" aria-label="Page Facebook de notre blog"><img src={facebooklogo} alt="HTML tutorial" style="width:42px;height:42px;"></a></li>
-                <li><a href="#" target="_blank" aria-label="Page Twitter de notre blog"><img src={twitterlogo} alt="HTML tutorial" style="width:42px;height:42px;"></a></li>
-                <li><a href="#" target="_blank" aria-label="Page Instagram de notre entreprise"><img src={instagramlogo} alt="HTML tutorial" style="width:42px;height:42px;"></a></li>
+                <li><a href="#" target="_blank" aria-label="Page Facebook de notre blog"><img src={facebooklogo} alt="Logo Facebook" style="width:42px;height:42px;"></a></li>
+                <li><a href="#" target="_blank" aria-label="Page Twitter de notre blog"><img src={twitterlogo} alt="Logo X (anciennement Twitter)" style="width:42px;height:42px;"></a></li>
+                <li><a href="#" target="_blank" aria-label="Page Instagram de notre entreprise"><img src={instagramlogo} alt="Logo Instagram" style="width:42px;height:42px;"></a></li>
             </ul>
             <ul class="linksfooter" >
-                <li><a href="/aboutus" title="Mentions légales" use:link>Mentions légales</a></li>
+                <li><a href="/legalnotice" title="Mentions légales" use:link>Mentions légales</a></li>
                 <li><a href="/aboutus" title="Page de contact" use:link>Contact</a></li>
             </ul>
         </nav>
